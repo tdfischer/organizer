a8dfa8be9b9181a37be595118ee0e111b0e4c2eb
Torrie Fischer <tdfischer@hackerbots.net>
2018-07-28 18:35:21 -0700

    components: loginsplash: fix redirect url, update to material-ui

--- assets/js/components/LoginSplash.js
+++ assets/js/components/LoginSplash.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { RaisedButton, Divider, Paper } from 'material-ui'
+import Button from '@material-ui/core/Button'
+import Divider from '@material-ui/core/Divider'
+import Paper from '@material-ui/core/Paper'
 import EBFESymbol from 'svg-react-loader!../../img/symbol.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -12,11 +14,12 @@ const LoginSplash = (_props) => (
         <p>East Bay for Everyone Organizer helps people organize for housing.</p>
         <Divider />
         <p />
-        <RaisedButton
-            href={window.SLACK_LOGIN_URL}
-            label="Sign in with EBFE Discuss"
-            icon={<EBFESymbol style={{width: 'auto', 'height': '80%'}}/>}
-        />
+        <Button
+            variant="contained"
+            href={window.LOGIN_URL}>
+            Sign in with EBFE Discuss
+            <EBFESymbol style={{width: 'auto', 'height': '80%'}}/>
+        </Button>
         <p>Please sign in with East Bay for Everyone Discuss to continue.</p>
         <p className="github-link"><a href="https://github.com/tdfischer/organizer/"><FontAwesomeIcon icon={['fab', 'github']} /></a></p>
     </Paper>
