8c9f98f0926a9dd996bef6ec06ede193fffb2794
Torrie Fischer <tdfischer@hackerbots.net>
2018-07-29 01:35:14 -0700

    reducers: select: dedupe code

--- assets/js/reducers/select.js
+++ assets/js/reducers/select.js
@@ -1,6 +1,24 @@
 import _ from 'lodash'
 import * as Select from '../store/select'
 
+function addSelection(state, action) {
+    return {
+        ...state,
+        selections: {
+            [action.key]: [..._.get(state.selections, action.key, []), action.item]
+        }
+    }
+}
+
+function removeSelection(state, action) {
+    return {
+        ...state,
+        selections: {
+            [action.key]: _.without(_.get(state.selections, action.key, []), action.item)
+        }
+    }
+}
+
 export default function selections(state = {}, action) {
     switch (action.type) {
     case Select.SET_SELECTION:
@@ -11,35 +29,15 @@ export default function selections(state = {}, action) {
             }
         }
     case Select.ADD_SELECTION:
-        return {
-            ...state,
-            selections: {
-                [action.key]: [..._.get(state.selections, action.key, []), action.item]
-            }
-        }
+        return addSelection(state, action)
     case Select.REMOVE_SELECTION:
-        return {
-            ...state,
-            selections: {
-                [action.key]: _.without(_.get(state.selections, action.key, []), action.item)
-            }
-        }
+        return removeSelection(state, action)
     case Select.TOGGLE_SELECTION: {
         const currentlySelected = _.get(state.selections, action.key, []).indexOf(action.item) != -1
         if (currentlySelected) {
-            return {
-                ...state,
-                selections: {
-                    [action.key]: _.without(_.get(state.selections, action.key, []), action.item)
-                }
-            }
+            return removeSelection(state, action)
         } else {
-            return {
-                ...state,
-                selections: {
-                    [action.key]: [..._.get(state.selections, action.key, []), action.item]
-                }
-            }
+            return addSelection(state, action)
         }
     }
     default:
