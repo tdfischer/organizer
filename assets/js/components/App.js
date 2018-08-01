280978b465c45b45195c8b4a02bfcc7b699717c8
Torrie Fischer <tdfischer@hackerbots.net>
2018-08-01 16:15:19 -0700

    components: app: add code splitting to login splash

--- assets/js/components/App.js
+++ assets/js/components/App.js
@@ -5,11 +5,11 @@ import importedComponent from 'react-imported-component'
 import { connect } from 'react-redux'
 import { hot } from 'react-hot-loader'
 
-import LoginSplash from './LoginSplash'
 import { getLoggedIn } from '../selectors/auth'
 import OrganizerAppBar from './OrganizerAppBar'
 import OrganizerBottomNav from './OrganizerBottomNav'
 
+const LoginSplash = importedComponent(() => import('./LoginSplash'))
 const MapIndex = importedComponent(() => import('./MapIndex'))
 const OrganizerDashboard = importedComponent(() => import('./OrganizerDashboard'))
 const PeopleIndex = importedComponent(() => import('./PeopleIndex'))
