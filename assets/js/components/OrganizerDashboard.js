f34f94da9edba50b4ff12d4880ce1fb24d13e7b3
Torrie Fischer <tdfischer@hackerbots.net>
2018-08-06 08:54:32 -0700

    components: organizerdashboard: hide confusing raw data behind a collapsable

--- assets/js/components/OrganizerDashboard.js
+++ assets/js/components/OrganizerDashboard.js
@@ -3,7 +3,10 @@ import { connect } from 'react-redux'
 import { Model } from '../store'
 import _ from 'lodash'
 import Typography from '@material-ui/core/Typography'
-import Divider from '@material-ui/core/Divider'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
+import Grid from '@material-ui/core/Grid'
 
 import { getCurrentUser } from '../selectors/auth'
 
@@ -15,20 +18,25 @@ class OrganizerDashboardBase extends React.Component {
     }
 
     render() {
-        const keys = _.keys(this.props.currentPerson)
-        const metadata = _.map(keys, k => {
-            return (<p key={k}>{k} = {JSON.stringify(_.get(this.props.currentPerson, k))}</p>)
-        })
-        console.log(this.props.currentPerson, metadata)
+        const metadata = JSON.stringify(this.props.currentPerson, null, 2)
         const name = _.get(this.props.currentPerson, 'name', '')
         return (
-            <div>
-                <Typography variant="title">{name}</Typography>
-                <Typography variant="subheading">{this.props.currentUser.email}</Typography>
-                <Divider />
-                <Typography variant="title">Raw data</Typography>
-                {metadata}
-            </div>
+            <Grid container direction="column" alignItems="stretch" spacing={8}>
+                <Grid item>
+                    <Typography variant="title">{name}</Typography>
+                    <Typography variant="subheading">{this.props.currentUser.email}</Typography>
+                </Grid>
+                <Grid item>
+                    <ExpansionPanel>
+                        <ExpansionPanelSummary>
+                            <Typography variant="title">Raw data</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <pre>{metadata}</pre>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                </Grid>
+            </Grid>
         )
     }
 }
