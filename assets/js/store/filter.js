dd34fd1ab3140adb1f5597b5e38eb30a3b5f1a42
Torrie Fischer <tdfischer@hackerbots.net>
2018-08-05 17:05:44 -0700

    store: filter: add support for custom matching function, defaulting to _.isMatch

--- assets/js/store/filter.js
+++ assets/js/store/filter.js
@@ -11,14 +11,15 @@ export const setFilter = (key, filter) => {
 }
 
 export default class Filterable {
-    constructor(key, reducer = _.identity) {
+    constructor(key, reducer = _.identity, matcher = _.isMatch) {
         this.key = key
         this.reducer = reducer
+        this.matcher = matcher
     }
 
     filtered(state, values) {
-        const regexp = _.get(state.filters.filters, this.key, '')
-        return _.filter(values, v => !!this.reducer(v).match(regexp))
+        const regexp = _.get(state.filters.filters, this.key)
+        return _.filter(values, v => this.matcher(this.reducer(v), regexp))
     }
 
     bindActionCreators(dispatch) {
