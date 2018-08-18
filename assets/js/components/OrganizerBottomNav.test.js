61c5104adff6705fff5f52599d2d45d4eed18334
Torrie Fischer <tdfischer@hackerbots.net>
2018-08-17 22:10:34 -0700

    components: organizerbottomnav: add tests for hiding staff buttons

--- assets/js/components/OrganizerBottomNav.test.js
+++ assets/js/components/OrganizerBottomNav.test.js
@@ -14,3 +14,14 @@ it('should navigate react-router when clicked', () => {
     wrapper.find('button').first().simulate('click')
     expect(clickHandler).toHaveBeenCalledTimes(1)
 })
+
+it('should correctly render staff buttons', () => {
+    const wrapper = shallow(<OrganizerBottomNav />)
+    const staffWrapper = shallow(<OrganizerBottomNav currentUser={{is_staff: true}} />)
+
+    expect(wrapper.find(BottomNavigationAction).find({value: '/captain'})).toHaveLength(0)
+    expect(wrapper.find(BottomNavigationAction).find({value: '/people'})).toHaveLength(0)
+
+    expect(staffWrapper.find(BottomNavigationAction).find({value: '/captain'})).toHaveLength(1)
+    expect(staffWrapper.find(BottomNavigationAction).find({value: '/people'})).toHaveLength(1)
+})
