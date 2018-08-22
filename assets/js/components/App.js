2d4de099668736efda118d2b5a2daf42ab127d50
Torrie Fischer <tdfischer@hackerbots.net>
2018-08-22 10:14:43 -0700

    ui: sentry: drop external sentry lib for a webpack bundled one, implement an error boundary to prevent a blank white screen being the crashed state

--- assets/js/components/App.js
+++ assets/js/components/App.js
@@ -10,12 +10,67 @@ import { history } from '../store'
 import { getLoggedIn } from '../selectors/auth'
 import OrganizerAppBar from './OrganizerAppBar'
 import OrganizerBottomNav from './OrganizerBottomNav'
+import { withStyles } from '@material-ui/core/styles'
+import Raven from 'raven-js'
+import { library as faLibrary } from '@fortawesome/fontawesome'
+import faTimes from '@fortawesome/fontawesome-free-solid/faTimes'
+import Button from '@material-ui/core/Button'
+
+faLibrary.add(faTimes)
 
 const LoginSplash = importedComponent(() => import('./LoginSplash'))
 const MapIndex = importedComponent(() => import('./MapIndex'))
 const OrganizerDashboard = importedComponent(() => import('./OrganizerDashboard'))
 const PeopleIndex = importedComponent(() => import('./PeopleIndex'))
 const CaptainIndex = importedComponent(() => import('./CaptainIndex'))
+
+class ErrorWrapperBase extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { error: null }
+    }
+
+    componentDidCatch(error, errorInfo) {
+        Raven.captureException(error, { extra: errorInfo })
+        console.error(error)
+        console.error(errorInfo)
+        this.setState({error})
+    }
+
+    render() {
+        if (this.state.error) {
+            return (
+                <div className={this.props.classes.root} onClick={() => Raven.lastEventId() && Raven.showReportDialog()}>
+                    <div className={this.props.classes.messagebox}>
+                        <i style={{color: 'red', width: 'auto', height: '8rem'}} className="fa fa-times" />
+                        <h1>Organizer has crashed!</h1>
+                        <p>This is quite unfortunate.</p>
+                        <p>{Raven.lastEventId() ? (<Button variant="contained" color="primary" onClick={() => Raven.showReportDialog()}>Report this bug</Button>) : null}</p>
+                        <em>{Raven.lastEventId() ? 'This error has been automatically reported' : 'This error could not be automatically reported'}</em>
+                        <pre className={this.props.classes.errorMessage}>{this.state.error.stack}</pre>
+                    </div>
+                </div>
+            )
+        } else {
+            return this.props.children
+        }
+    }
+}
+
+const errorStyles = {
+    root: {
+        width: '80%',
+        margin: 'auto',
+        textAlign: 'center',
+        paddingTop: '5rem'
+    },
+    errorMessage: {
+        overflow: 'auto',
+        backgroundColor: '#ddd'
+    }
+}
+
+const ErrorWrapper = withStyles(errorStyles)(ErrorWrapperBase)
 
 export const App = props => (
     props.logged_in ? (
@@ -45,7 +100,7 @@ const mapStateToProps = state => {
 
 const RouterApp = (props) => (
     <ConnectedRouter history={props.history}>
-        <App {...props} />
+        <ErrorWrapper><App {...props} /></ErrorWrapper>
     </ConnectedRouter>
 )
 
