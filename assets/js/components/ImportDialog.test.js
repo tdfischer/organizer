f2aced253b5084f3cdb92936a57b43e9d67a84a7
Torrie Fischer <tdfischer@hackerbots.net>
2018-09-12 01:26:28 -0700

    components: importdialog: mock up fetch for test

--- assets/js/components/ImportDialog.test.js
+++ assets/js/components/ImportDialog.test.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import ImportDialog from './ImportDialog'
+import fetchMock from 'fetch-mock'
 
 it('should render defaults safely', () => {
+    fetchMock.mock('/api/people/', {})
     shallow(<ImportDialog />)
 })
 
