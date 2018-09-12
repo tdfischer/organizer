9370fb361f0dccbdc5b33e54384bb33c8b02c820
Torrie Fischer <tdfischer@hackerbots.net>
2018-09-12 10:39:37 -0700

    js: django: fixup cookie parsing which is somehow different on heroku

--- assets/js/Django.js
+++ assets/js/Django.js
@@ -1,8 +1,8 @@
 const getCookie = name => {
     const cookie = document.cookie
         .split(';')
-        .find(cookie => cookie.startsWith(name + '=')) || ''
-    return decodeURIComponent(cookie.substring(name.length + 1))
+        .find(cookie => cookie.trim().startsWith(name + '=')) || ''
+    return decodeURIComponent(cookie.trim().substring(name.length + 1))
 }
 
 export var csrftoken = getCookie('csrftoken')
