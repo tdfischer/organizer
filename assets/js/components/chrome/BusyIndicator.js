7b4da2925423d164ff3ffbd1a25cb07403e03b51
Torrie Fischer <tdfischer@hackerbots.net>
2018-09-25 13:01:51 -0400

    tests: components: chrome: reach 100% test coverage on chrome elements

--- assets/js/components/chrome/BusyIndicator.js
+++ assets/js/components/chrome/BusyIndicator.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import CircularProgress from '@material-ui/core/CircularProgress'
-import { getSaving, getLoading } from '../../selectors'
+import { getActivityCount } from '../../selectors'
 
 const mapStateToProps = state => {
     return {
-        queueSize: getSaving(state) + getLoading(state)
+        queueSize: getActivityCount(state)
     }
 }
 
-const BusyIndicator = connect(mapStateToProps)(props => (
+export const BusyIndicator = props => (
     (props.queueSize > 0) ? <CircularProgress color="secondary" /> : null
-))
+)
 
-export default BusyIndicator
+export default connect(mapStateToProps)(BusyIndicator)
