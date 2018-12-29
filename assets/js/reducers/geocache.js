c641aa0f02197e6be59778090f1508082cbaf471
Torrie Fischer <tdfischer@hackerbots.net>
2018-12-29 11:55:40 -0800

    js: geocache: add geolocation accuracy into event relevance and distance calculations

--- assets/js/reducers/geocache.js
+++ assets/js/reducers/geocache.js
@@ -4,13 +4,14 @@ import Immutable from 'immutable'
 export default function(state = Immutable.Map(), action = {}) {
     switch (action.type) {
     case Actions.Geocache.UPDATE_CURRENT_LOCATION:
-        return state.set('currentLocation', action.geo)
+        return state.set('accuracy', action.accuracy).set('currentLocation', action.geo)
     case Actions.Geocache.SET_LOCATION_STATUS:
         return state.set('status', action.status)
     default:
         return Immutable.Map({
             currentLocation: undefined,
             status: undefined,
+            accuracy: undefined
         }).merge(state)
     }
 }
--- assets/js/selectors/geocache.js
+++ assets/js/selectors/geocache.js
@@ -1,5 +1,6 @@
 import { point } from '@turf/helpers'
 
 export const getCurrentLocation = state => state.getIn(['geocache', 'currentLocation']) ? point(state.getIn(['geocache', 'currentLocation'])) : null
+export const getAccuracy = state => state.getIn(['geocache', 'accuracy'])
 
 export const getLocationStatus = state => state.getIn(['geocache', 'status'])
