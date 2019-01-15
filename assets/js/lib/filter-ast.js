93e3a17013199b4dcc22bbcd5a19ae6b8ab8429e
Torrie Fischer <tdfischer@hackerbots.net>
2019-01-14 22:52:52 -0800

    js: lib: filter-ast: drop lodash for native code

--- assets/js/lib/filter-ast.js
+++ assets/js/lib/filter-ast.js
@@ -1,10 +1,8 @@
-import _ from 'lodash'
-
 export const matchOrContains = (needle, haystack) => {
     if (typeof(haystack) == 'string') {
         return haystack.indexOf(needle) != -1
     } else if (haystack instanceof Array) {
-        return _.find(haystack, stack => matchOrContains(needle, stack)) != undefined
+        return haystack.find(stack => matchOrContains(needle, stack)) != undefined
     } else {
         return false
     }
@@ -33,32 +31,32 @@ export const makeComparator = (obj) => {
         }
         switch (op) {
         case 'contains':
-            return (row) => matchOrContains(value, _.get(row, property))
+            return (row) => matchOrContains(value, row[property])
         case 'is':
-            return (row) => isEqual(value, _.get(row, property))
+            return (row) => isEqual(value, row[property])
         case 'gt':
-            return floatCmp((row, asFloat) => _.get(row, property) > asFloat, value)
+            return floatCmp((row, asFloat) => row[property] > asFloat, value)
         case 'gte':
-            return floatCmp((row, asFloat) => _.get(row, property) >= asFloat, value)
+            return floatCmp((row, asFloat) => row[property] >= asFloat, value)
         case 'lt':
-            return floatCmp((row, asFloat) => _.get(row, property) < asFloat, value)
+            return floatCmp((row, asFloat) => row[property] < asFloat, value)
         case 'lte':
-            return floatCmp((row, asFloat) => _.get(row, property) <= asFloat, value)
+            return floatCmp((row, asFloat) => row[property] <= asFloat, value)
         default:
             throw Error('Unknown operator ' + op)
         }
     } else {
-        const comparators = _.map(children, makeComparator)
+        const comparators = children.map(makeComparator)
         if (op == 'and') {
             // Run through each comparator, returning the first one that is
             // false; if one such comparator exists (find doesn't return
             // undefined), fail the match
-            return obj => (_.find(comparators, comparator => !comparator(obj)) == undefined)
+            return obj => (comparators.find(comparator => !comparator(obj)) == undefined)
         } else if (op == 'or') {
             // Run through each comparator, returning the first one that is
             // true; if no such comparator exists (find returns undefined), fail
             // the match
-            return obj => (_.find(comparators, comparator => comparator(obj)) != undefined)
+            return obj => (comparators.find(comparator => comparator(obj)) != undefined)
         }
     }
 }
