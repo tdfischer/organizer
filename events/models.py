8e4604dfb162c65133c3b21d79b45853923955e9
Torrie Fischer <tdfischer@hackerbots.net>
2019-05-10 16:55:27 -0700

    events: migrate to locations
    includes work around for django bug #23577, against django developer advice

--- events/models.py
+++ events/models.py
@@ -2,22 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from address.models import AddressField
 from django.utils import timezone
 from crm.models import Person
 from crm import geocache
-import django_rq
+from geocodable.models import LocationAlias
 import uuid
-
-@django_rq.job
-def updateEventGeo(eventID):
-    event = Event.objects.get(pk=eventID)
-    if event.location is not None:
-        resolved = geocache.geocode(event.location.raw)
-        event.location = resolved
-        event.lng = resolved.get('lng')
-        event.lat = resolved.get('lat')
-        event.save(_updateGeocache=False)
 
 class Event(models.Model):
     name = models.CharField(max_length=200)
@@ -25,23 +14,27 @@ class Event(models.Model):
     end_timestamp = models.DateTimeField()
     attendees = models.ManyToManyField(Person, related_name='events', blank=True)
     uid = models.CharField(max_length=200, blank=True)
-    location = AddressField(null=True, default=None, blank=True)
-    lat = models.FloatField(null=True, blank=True)
-    lng = models.FloatField(null=True, blank=True)
+    location = models.ForeignKey(LocationAlias, default=None, blank=True,
+            null=True)
     instance_id = models.CharField(max_length=200, blank=True)
 
     @property
     def geo(self):
         return {'lat': self.lat, 'lng': self.lng}
 
-    def save(self, *args, **kwargs):
-        runUpdate = kwargs.pop('_updateGeocache', True)
-        super(Event, self).save(*args, **kwargs)
-        if runUpdate:
-            self.queue_geocache_update()
+    @property
+    def lat(self):
+        if self.location is not None:
+            return self.location.lat
+        else:
+            return None
 
-    def queue_geocache_update(self):
-        updateEventGeo.delay(self.id)
+    @property
+    def lng(self):
+        if self.location is not None:
+            return self.location.lng
+        else:
+            return None
 
     def __unicode__(self):
         return "%s (%s)"%(self.name, self.timestamp)
