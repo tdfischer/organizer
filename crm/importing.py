4a3a7571886e0840ecfaaabb28e2ee2c08c4efe6
Torrie Fischer <tdfischer@hackerbots.net>
2019-08-01 15:29:14 -0700

    crm: importing: reimplement tag fields with a better understanding of m2m relationships in django-import-export

--- crm/importing.py
+++ crm/importing.py
@@ -12,7 +12,10 @@ from mailchimp3 import MailChimp
 from django import forms
 import requests
 
-class CommaSeparatedListWidget(widgets.Widget):
+class CommaSeparatedListWidget(widgets.ManyToManyWidget):
+    def __init__(self, *args, **kwargs):
+        super(CommaSeparatedListWidget, self).__init__(*args, model=None, **kwargs)
+
     def clean(self, value, row=None, *args, **kwargs):
         return value.split(',')
 
@@ -23,13 +26,34 @@ class CommaSeparatedListWidget(widgets.Widget):
             return ''
         return ','.join(i)
 
-class TagField(fields.Field):
+class TagSetField(fields.Field):
+    def __init__(self, append=True, remove=False, replace=False, *args,
+            **kwargs):
+        super(TagSetField, self).__init__(*args, **kwargs)
+        self.append = append
+        self.remove = remove
+        self.replace = replace
+
     def export(self, obj):
-        return obj.tags.names()
+        value = self.get_value(obj)
+        if value is None:
+            return ''
+        return self.widget.render(value.names(), obj)
 
     def save(self, obj, data, is_m2m=False):
-        print "Adding %s to %s"%(data, obj)
-        obj.tags.add(data['tags'])
+        if not self.readonly and is_m2m:
+            attrs = self.attribute.split('__')
+            for attr in attrs[:-1]:
+                obj = getattr(obj, attr, None)
+            cleaned = self.clean(data)
+            if cleaned is not None or self.saves_null_values:
+                if self.append:
+                    getattr(obj, attrs[-1]).add(*cleaned)
+                elif self.remove:
+                    getattr(obj, attrs[-1]).remove(*cleaned)
+                elif self.replace:
+                    getattr(obj, attrs[-1]).set(*cleaned)
+
 
 class PersonResource(resources.ModelResource):
     location = fields.Field(
@@ -39,7 +63,7 @@ class PersonResource(resources.ModelResource):
         saves_null_values = False
     )
 
-    tags = TagField(
+    tags = TagSetField(
         column_name = 'tags',
         attribute = 'tags',
         widget=CommaSeparatedListWidget(),
