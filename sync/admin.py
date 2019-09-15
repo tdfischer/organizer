c1303a300283d026af603a031fee15e33d263cbd
Torrie Fischer <tdfischer@hackerbots.net>
2019-09-15 16:39:02 -0700

    sync: admin: rewrite import admin to support export as well, add export sinks to admin

--- sync/admin.py
+++ sync/admin.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from . import models, forms
 from django.conf.urls import url
 from django.contrib import admin
-from organizer import importing 
+from organizer import importing, exporting
 from django.template.response import TemplateResponse
 from django.core.urlresolvers import reverse
 from django.http.response import HttpResponseRedirect
@@ -23,7 +23,7 @@ def disable_target(modeladmin, request, queryset):
         target.save()
 disable_target.short_description = "Disable selected targets"
 
-class SyncAdmin(admin.ModelAdmin):
+class PluginModelAdmin(admin.ModelAdmin):
     list_filter = ['enabled', 'backend']
     list_display = [
         'name', 'backend', 'enabled', 'lastRun'
@@ -34,12 +34,22 @@ class SyncAdmin(admin.ModelAdmin):
         disable_target,
     )
 
+    def get_form(self, request, obj=None, **kwargs):
+        kwargs['form'] = forms.ImportSourceForm
+        return super(PluginModelAdmin, self).get_form(request, obj, **kwargs)
+
+    def get_options_form(self, backend_name):
+        pluginCls = self.plugin_class.get_plugin(backend_name)
+        if pluginCls is None:
+            return self.default_config_form
+        return pluginCls({}).options_form
+
     def get_urls(self):
-        urls = super(SyncAdmin, self).get_urls()
+        urls = super(PluginModelAdmin, self).get_urls()
+        info = self.model._meta.app_label, self.model._meta.model_name
         return [
-            #url('^add', self.select_backend_view, name='sync_synctarget_add'),
-            url('^add/(?P<backend_name>.+)', self.configure_and_add_view, name='sync_configure_and_add'),
-            #url('^(?P<pk>\d+)/change', self.configure_backend_view)
+            url('^add/(?P<backend_name>.+)', self.configure_and_add_view,
+                name='%s_%s_add'%(info))
         ] + urls
 
     def configure_and_add_view(self, request, backend_name):
@@ -50,31 +60,28 @@ class SyncAdmin(admin.ModelAdmin):
         form = None
         initial_config = {}
         extra_context = extra_context or {}
-        formConstructor = forms.DefaultImportSourceConfigForm
 
         if object_id is not None:
-            source = models.ImportSource.objects.get(pk=object_id)
-            initial_config = json.loads(source.configuration)
-            backend_name = source.backend
+            source = self.model.objects.get(pk=object_id)
+            initial_config = json.loads(getattr(source, self.plugin_config_field))
+            backend_name = getattr(source, self.plugin_name_field)
 
+        # If we have no backend, and we have no source id, 404
         if backend_name is None and source is None:
             raise Http404()
 
-        importerCls = importing.get_importer_class(backend_name)
-        importerInstance = None
-        if importerCls is not None:
-            importerInstance = importerCls(initial_config)
-            formConstructor = importerInstance.options_form
+        formConstructor = self.get_form(request, source)
+        configFormConstructor = self.get_options_form(backend_name)
 
         if request.method == 'POST':
-            base_form = forms.ImportSourceForm(request.POST, prefix='base',
+            base_form = formConstructor(request.POST, prefix='base',
                     instance=source, initial={'backend': backend_name})
-            form = formConstructor(request.POST,
+            form = configFormConstructor(request.POST,
                     initial=initial_config, prefix='options')
         else:
-            base_form = forms.ImportSourceForm(prefix='base', instance=source,
+            base_form = formConstructor(prefix='base', instance=source,
                     initial={'backend': backend_name})
-            form = formConstructor(prefix='options', initial=initial_config)
+            form = configFormConstructor(prefix='options', initial=initial_config)
         
         if form.is_valid() and base_form.is_valid():
             isNew = False
@@ -83,8 +90,8 @@ class SyncAdmin(admin.ModelAdmin):
             change_message = self.construct_change_message(request, form, [],
                     isNew)
             source = base_form.save(commit=False)
-            source.backend = backend_name
-            source.configuration = json.dumps(form.cleaned_data)
+            setattr(source, self.plugin_name_field, backend_name)
+            setattr(source, self.plugin_config_field, json.dumps(form.cleaned_data))
             source.save()
             if isNew:
                 self.log_addition(request, source, change_message)
@@ -96,28 +103,36 @@ class SyncAdmin(admin.ModelAdmin):
         extra_context['backend_name'] = backend_name
         extra_context['base_form'] = base_form
         extra_context['has_backend'] = True
-        return super(SyncAdmin, self).change_view(request, object_id, form_url,
+        return super(PluginModelAdmin, self).change_view(request, object_id, form_url,
                 extra_context)
-
-        context = dict(
-            self.admin_site.each_context(request),
-            options_form=form,
-            base_form=base_form
-        )
-        return TemplateResponse(request, "configure.html", context)
 
     def render_change_form(self, request, context, add=False, change=False,
             form_url='', obj=None):
         if 'has_backend' in context:
             add = False
-        return super(SyncAdmin, self).render_change_form(request, context,
+        return super(PluginModelAdmin, self).render_change_form(request, context,
                 add, change, form_url, obj)
 
 
     add_form_template = 'admin/sync/add.html'
     def add_view(self, request, form_url='', extra_context=None):
         extra_context = extra_context or {}
-        extra_context['backends'] = importing.get_importer_classes()
-        return super(SyncAdmin, self).add_view(request, form_url, extra_context)
+        extra_context['backends'] = self.plugin_class.plugins
+        info = self.model._meta.app_label, self.model._meta.model_name
+        extra_context['add_url'] = 'admin:%s_%s_add'%(info)
+        return super(PluginModelAdmin, self).add_view(request, form_url, extra_context)
 
-admin.site.register(models.ImportSource, SyncAdmin)
+class ImportAdmin(PluginModelAdmin):
+    plugin_class = importing.DatasetImporter
+    plugin_name_field = 'backend'
+    plugin_config_field = 'configuration'
+    default_config_form = forms.DefaultImportSourceConfigForm
+
+class ExportAdmin(PluginModelAdmin):
+    plugin_class = exporting.DatasetExporter
+    plugin_name_field = 'backend'
+    plugin_config_field = 'configuration'
+    default_config_form = forms.DefaultExportSinkConfigForm
+
+admin.site.register(models.ImportSource, ImportAdmin)
+admin.site.register(models.ExportSink, ExportAdmin)
--- sync/forms.py
+++ sync/forms.py
@@ -7,5 +7,14 @@ class ImportSourceForm(forms.ModelForm):
         model = models.ImportSource
         fields = ['name', 'enabled', 'backend']
 
+class ExportSinkForm(forms.ModelForm):
+    backend = forms.CharField(disabled=True)
+    class Meta:
+        model = models.ExportSink
+        fields = ['name', 'enabled', 'backend']
+
 class DefaultImportSourceConfigForm(forms.Form):
+    pass
+
+class DefaultExportSinkConfigForm(forms.Form):
     pass
