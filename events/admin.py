4e3197a618f758b167525d99decdc25487ee1639
Torrie Fischer <tdfischer@hackerbots.net>
2019-12-16 01:17:32 -0800

    events: admin: add support for merging events

--- events/admin.py
+++ events/admin.py
@@ -5,8 +5,26 @@ from django.contrib import admin
 from . import models
 from organizer.admin import admin_site, OrganizerModelAdmin
 from rangefilter.filter import DateTimeRangeFilter
+from crm.models import merge_models
+from django.db import transaction
+
+def merge_events(modeladmin, request, queryset):
+    first = queryset[0]
+    duplicates = queryset[1:]
+    merged, relations = merge_models(first, *list(duplicates))
+    with transaction.atomic():
+        for d in duplicates:
+            d.delete()
+        for r in relations:
+            r.save()
+        merged.save()
+merge_events.short_description = "Merge selected events and their attendee lists"
 
 class EventAdmin(OrganizerModelAdmin):
+    actions = (
+        merge_events,
+    )
+
     search_fields = [
         'name', 'location__raw'
     ]
